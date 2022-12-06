97750097ccf4c069194ce64a0195045f48b5de05
JayP718 <jaypolanco42@gmail.com>
2022-12-05 22:33:21 -0500

    first commit

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,21 +1,35 @@
 <script>
-  import { getContext } from "svelte"
+  import { getContext, onMount } from "svelte";
 
-  export let text
-  export let pop_position
-  export let title
+  export let ranking;
+   
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
-  const uuid = Math.random()
-</script>        
+  $: error = ranking < 1 ? "Please assign a ranking greater than 0" : false;
+
+
+  //Shepherd walk-through step picks up attribute once the complete Dom has loaded
+
+  const { styleable } = getContext("sdk");
+  const component = getContext("component");
+ 
+</script>
 
 <div use:styleable={$component.styles}>
-  <div class="sheep_step" id={uuid} data-pos={pop_position} data-text={text} data-title={title}>
-    i 
-    <slot/>
-  </div>   
+  {#if error}
+    <div class="error">{error}</div>
+  {:else}
+    <div id={$component.id}>
+      <slot />
+    </div>
+  {/if}
 </div>
-
-
- 
+    
+<style>
+  .error {
+    color: white;
+    font-size: var(--spectrum-global-dimension-font-size-75);
+    margin-top: var(--spectrum-global-dimension-size-75);
+    background: red;
+  } 
+</style> 
+    
