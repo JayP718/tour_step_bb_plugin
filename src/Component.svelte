15de0594fced22a5e3a77117a513a96753243579
JayP718 <jaypolanco42@gmail.com>
2022-12-09 08:01:46 -0500

    reordered some of the schema.json

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,19 +1,15 @@
 <script>
   import { getContext } from "svelte";
-
   export let ranking;
    
-
   $: error = ranking < 1 ? "Please assign a ranking greater than 0" : false;
 
-
   //Shepherd walk-through step picks up attribute once the complete Dom has loaded
-
-  const { styleable } = getContext("sdk");
+  const { styleable} = getContext("sdk");
   const component = getContext("component");
- 
-</script>
 
+</script>
+ 
 <div use:styleable={$component.styles}>
   {#if error}
     <div class="error">{error}</div>
